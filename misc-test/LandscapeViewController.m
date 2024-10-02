ff112109aa2e2816227f9aff848146ebf0e5781e
Pedro Hernández <pedrohdezmlg@gmail.com>
2024-10-02 09:59:56 +0200

    Initial commit

---
+++ misc-test/LandscapeViewController.m
@@ -0,0 +1,13 @@
+#import "LandscapeViewController.h"
+
+@interface LandscapeViewController ()
+
+@end
+
+@implementation LandscapeViewController
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscapeLeft;
+}
+
+@end
